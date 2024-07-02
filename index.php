3351cf2b124c4e13f0aa68176dd73e576402a409
LukaszWasiluk01 <lukasz.wasiluk01@gmail.com>
2024-07-02 17:43:28 +0200

    dodanie szkieletu index.php wraz ze stylami

--- index.php
+++ index.php
@@ -1,11 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Menedżer zakładek</title>
+    <link rel="stylesheet" href="./Styles/styles.css">
 </head>
-<body>
 
+<body>
+    <header>
+        <nav class="navbar">
+            <a href="index.php">Moje zakładki</a>
+            <div class="dropdown">
+                <button class="dropbtn">Nazwa_uzytkownika</button>
+                <div class="dropdown-content">
+                    <a href="grupy-zakladek.php">Moje grupy zakladek</a>
+                    <a href="znajomi.php">Znajomi</a>
+                    <a href="zaproszenia.php">Oczekujące zaproszenia</a>
+                    <a href="preferencje.php">Moje preferencje</a>
+                    <a href="logout.php">Wyloguj</a>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <main>
+    </main>
+    <footer>
+        <p>&copy; 2024 Łukasz Wasiluk</p>
+    </footer>
 </body>
+
 </html>
